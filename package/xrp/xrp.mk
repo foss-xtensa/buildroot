c6a0f40cff4f066e70004e0c5811752133632687
William Tambe <williamt@cadence.com>
2022-12-22 07:28:35 -0800

    package/xrp: updates to latest version

--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XRP_VERSION = 9d7b35eedb71585d7543d91a220b01d2ef850b2d
+XRP_VERSION = 2f5c7816aa47950fa25da6e850b9c68e4f385f7d
 XRP_SITE = $(call github,foss-xtensa,xrp,$(XRP_VERSION))
 XRP_SOURCE = xrp-$(XRP_VERSION).tar.gz
 BR_NO_CHECK_HASH_FOR += $(XRP_SOURCE)
