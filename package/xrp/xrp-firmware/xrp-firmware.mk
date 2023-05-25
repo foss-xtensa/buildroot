faafde744c926154edb3979f068c65f8c3193eb4
William Tambe <williamt@cadence.com>
2023-05-24 20:57:22 -0700

    package/xrp: noautotools changes

--- package/xrp/xrp-firmware/xrp-firmware.mk
+++ package/xrp/xrp-firmware/xrp-firmware.mk
@@ -9,59 +9,15 @@ XRP_FIRMWARE_SITE = $(XRP_SITE)
 XRP_FIRMWARE_SOURCE = $(XRP_SOURCE)
 XRP_FIRMWARE_LICENSE = MIT
 
-XRP_FIRMWARE_CONF_ENV = CC="xt-clang --xtensa-core=$(call qstrip,$(BR2_PACKAGE_$(PKG)_DSP_CORE_NAME))" AR="xt-ar --xtensa-core=$(call qstrip,$(BR2_PACKAGE_$(PKG)_DSP_CORE_NAME))" RANLIB="xt-ranlib --xtensa-core=$(call qstrip,$(BR2_PACKAGE_$(PKG)_DSP_CORE_NAME))"
-
-XRP_FIRMWARE_CONF_OPTS = --disable-host --enable-dsp --enable-hosted --disable-standalone --disable-single
-
-ifeq ($(BR2_PACKAGE_XRP_FIRMWARE_EXAMPLE),y)
-XRP_FIRMWARE_CONF_OPTS += --enable-example
-XRP_FIRMWARE_CONF_OPTS += --enable-port=simple-xos
-#XRP_FIRMWARE_CONF_OPTS += CFLAGS=-DDEBUG
-endif
-
-define XRP_FIRMWARE_CONFIGURE_CMDS
-	(cd $($(PKG)_SRCDIR) && rm -rf config.cache && \
-	$($(PKG)_CONF_ENV) \
-	XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS))/../config" \
-	PATH="$(call qstrip,$(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS)):$$PATH" \
-	CONFIG_SITE=/dev/null \
-	./configure \
-		--host=xtensa-elf \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--sysconfdir=/etc \
-		--localstatedir=/var \
-		--program-prefix="" \
-		--disable-dependency-tracking \
-		$(QUIET) $($(PKG)_CONF_OPTS) \
-	)
-endef
-
-XRP_FIRMWARE_IMAGE_NAME = xrp-dsp-hosted
-
 define XRP_FIRMWARE_BUILD_CMDS
-  XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS))/../config" \
-  $(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS)/../libexec/xt-mbuild \
-	-root $(@D) -socfile $(BR2_PACKAGE_XRP_FIRMWARE_DSP_LSP_XTSYS) \
-	-syspkg $(@D)/xrp-example/soc/package \
-	-sysbuild $(@D)/xrp-example/soc/build \
-	-preclean -build sw -lsps sim,sim-stacklocal ; \
-  DSP_COMM_BASES=($(call qstrip,$(BR2_PACKAGE_$(PKG)_DSP_COMM_BASE))) ; \
-  for (( CORE=0; CORE < $(BR2_PACKAGE_$(PKG)_DSP_NUM_CORES); ++CORE )) ; do \
-    $(MAKE) \
-	$($(PKG)_CONFIG-y) \
-	XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS))/../config" \
-	PATH="$(call qstrip,$(BR2_PACKAGE_$(PKG)_XTENSA_TOOLS)):$$PATH" \
-	-C $(@D) \
-	DSP_LSP=`printf $(BR2_PACKAGE_$(PKG)_DSP_LSP) $${CORE}` \
-	DSP_COMM_BASE=$${DSP_COMM_BASES[$${CORE}]} && \
-    mv $(@D)/xrp-example/$($(PKG)_IMAGE_NAME){,$$CORE} ; \
-  done
+  XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_XTENSA_TOOLS))/../config" \
+  XTENSA_CORE="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_DSP_CORE_NAME))" \
+  $(MAKE) -j1 -C $(@D)/xrp-example XRP_DSP_USE_SRCS=1 XRP_DSP_HW_PORT=simple-xos xrpdsp
 endef
 
 define XRP_FIRMWARE_INSTALL_TARGET_CMDS
-  for (( CORE=0 ; CORE < $(BR2_PACKAGE_$(PKG)_DSP_NUM_CORES); ++CORE )) ; do \
-    $(INSTALL) -D -m 0644 $(@D)/xrp-example/$($(PKG)_IMAGE_NAME)$${CORE} $(TARGET_DIR)/lib/firmware/xrp$${CORE}.elf ; \
+  for (( CORE=0 ; CORE < $(BR2_PACKAGE_XRP_FIRMWARE_DSP_NUM_CORES); ++CORE )) ; do \
+    $(INSTALL) -D -m 0644 $(@D)/xrp-example/xrp-dsp-standalone$${CORE} $(TARGET_DIR)/lib/firmware/xrp$${CORE}.elf ; \
   done
 endef
 
--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -4,19 +4,26 @@
 #
 ################################################################################
 
-XRP_VERSION = 2f5c7816aa47950fa25da6e850b9c68e4f385f7d
+XRP_VERSION = 2c453817585ca51e8b0b24ca9060241d46853e05
 XRP_SITE = $(call github,foss-xtensa,xrp,$(XRP_VERSION))
 XRP_SOURCE = xrp-$(XRP_VERSION).tar.gz
-BR_NO_CHECK_HASH_FOR += $(XRP_SOURCE)
 XRP_LICENSE = MIT, GPL
 
-XRP_CONF_OPTS += --disable-dsp --disable-standalone --disable-single --enable-hosted --enable-example
+define XRP_BUILD_CMDS
+  XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_XTENSA_TOOLS))/../config" \
+  XTENSA_CORE="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_DSP_CORE_NAME))" \
+  $(MAKE) -j1 $(TARGET_CONFIGURE_OPTS) -C $(@D)/xrp-example xrphosted
+endef
+
+define LIBFOO_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0755 $(@D)/xrp-example/xrp-host-hosted $(TARGET_DIR)/usr/bin/
+endef
 
 XRP_MODULE_SUBDIRS = xrp-kernel
 XRP_MODULE_MAKE_OPTS = CONFIG_XRP=m CONFIG_XRP_HW_SIMPLE=m
 #XRP_MODULE_MAKE_OPTS += CONFIG_XRP_DEBUG=y
 
 $(eval $(kernel-module))
-$(eval $(autotools-package))
+$(eval $(generic-package))
 
 include $(sort $(wildcard package/xrp/*/*.mk))
