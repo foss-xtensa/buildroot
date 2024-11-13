c743e6db98f34253a27422f0dfc8a6cee49b845f
William Tambe <williamt@cadence.com>
2024-11-13 14:11:25 -0800

    package/xrp: fixes and updates

--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -4,18 +4,18 @@
 #
 ################################################################################
 
-XRP_VERSION = 2c453817585ca51e8b0b24ca9060241d46853e05
+XRP_VERSION = 189808909f625f2c7b724cb9dabe0e95d606ce6d
 XRP_SITE = $(call github,foss-xtensa,xrp,$(XRP_VERSION))
 XRP_SOURCE = xrp-$(XRP_VERSION).tar.gz
 XRP_LICENSE = MIT, GPL
 
 define XRP_BUILD_CMDS
-  XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_XTENSA_TOOLS))/../config" \
+  XTENSA_SYSTEM="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_XTENSA_SYSTEM))" \
   XTENSA_CORE="$(call qstrip,$(BR2_PACKAGE_XRP_FIRMWARE_DSP_CORE_NAME))" \
   $(MAKE) -j1 $(TARGET_CONFIGURE_OPTS) -C $(@D)/xrp-example xrphosted
 endef
 
-define LIBFOO_INSTALL_TARGET_CMDS
+define XRP_INSTALL_TARGET_CMDS
   $(INSTALL) -D -m 0755 $(@D)/xrp-example/xrp-host-hosted $(TARGET_DIR)/usr/bin/
 endef
 
